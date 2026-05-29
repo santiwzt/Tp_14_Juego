e5a52a4c8fb0205446f57f061c15d8124738a680
santiwzt <santi.wzt@gmail.com>
2026-05-29 10:39:00 -0300

    Segunda Clase

---
+++ Assets/Scripts/ContadorObjetos.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ContadorObjetos : MonoBehaviour
+{
+    public int objetosRecolectados = 0;
+    public TextMeshProUGUI textoContador;
+
+    void Start()
+    {
+        ActualizarTexto();
+    }
+
+    public void SumarObjeto()
+    {
+        objetosRecolectados++;
+        ActualizarTexto();
+    }
+
+    void ActualizarTexto()
+    {
+        textoContador.text = "Objetos: " + objetosRecolectados;
+    }
+}
